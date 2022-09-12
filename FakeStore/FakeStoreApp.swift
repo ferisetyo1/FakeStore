d39f3ef19c3a35b8f7efee4280c7d06b88fb48eb
Feri Setyo Efendi <ferisetyo1@gmail.com>
2022-09-12 18:04:22 +0700

    Initial Commit

---
+++ FakeStore/FakeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApp.swift
+//  FakeStore
+//
+//  Created by feri setyo on 12/09/22.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
